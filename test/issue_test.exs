1e6a46a4093c2ecac8153a3df3ea1c6d67ee80d3
JJ Merelo <jjmerelo@gmail.com>
2021-10-27 07:31:47 +0200

    Eliminates unused doctest

--- test/issue_test.exs
+++ test/issue_test.exs
@@ -1,12 +1,11 @@
 defmodule IssueTest do
   use ExUnit.Case
-  doctest Issue
 
   setup_all do
     this_issue = %Issue{ projectname: 'Foo', id: '1'}
     {:ok, issue: this_issue}
   end
-  
+
   test "Initial issue state",context do
     assert context[:issue].state == :Open
   end
